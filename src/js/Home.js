f0a05e82af9d91a53c65fffe72c53040232c265b
diazmoraj <103903062+diazmoraj@users.noreply.github.com>
2024-04-02 01:08:38 +0000

    Modificacion de archivo home.jsx a Home.js

--- src/js/Home.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import Navbar from "./Navbar";
-import Card from "./Card";
-import Jumbotron from "./Jumbotron";
-
-export default Home;
---
+++ src/js/component/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import Navbar from "../Navbar";
+import Card from "../Card";
+import Jumbotron from "../Jumbotron";
+
+export default Home;
