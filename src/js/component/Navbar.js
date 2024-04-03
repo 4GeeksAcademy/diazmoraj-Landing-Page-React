86e27a91d4b3eff66976b1ba599c889fef990b52
diazmoraj <103903062+diazmoraj@users.noreply.github.com>
2024-04-03 00:59:55 +0000

    Actualziacion de Navbar

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="#">Start Bootstrap</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -14,13 +14,13 @@ const Navbar = () => {
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Features</a>
+                            <a class="nav-link active" href="#">About</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Pricing</a>
+                            <a class="nav-link active" href="#">Services</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                            <a class="nav-link active" aria-disabled="true">Contact</a>
                         </li>
                     </ul>
                 </div>
