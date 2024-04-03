0013d63d476e62882bad073315c6ae9a0e47b347
diazmoraj <103903062+diazmoraj@users.noreply.github.com>
2024-04-03 04:17:54 +0000

    Actualizacion de boton

--- src/js/component/Jumbotron.js
+++ src/js/component/Jumbotron.js
@@ -4,14 +4,13 @@ const Jumbotron = () => {
     return (
         <div className="container bg-light bg-gradient m-4 p-4">
             <h1>A Warm Welcome!</h1>
-            <p>
+            <p className="text-start">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore esse eius
                 impedit consequuntur repellendus autem fugiat nobis veritatis voluptatibus fugit
-                aut neque suscipit harum, voluptas magni maxime? Totam iure enim quia commodi ducimus
-                aliquid porro consectetur in quibusdam voluptate sunt et reiciendis omnis nam laborum,
-                ex architecto minima eum? Amet?
+                aut neque suscipit harum.Inventore esse eius impedit consequuntur repellendus autem
+                fugiat nobis veritatis voluptatibus fugit aut neque suscipit harum.
             </p>
-            <button type="button" class="btn btn-primary ">Call to action!</button>
+            <button type="button" class="btn btn-primary d-md-flex">Call to action!</button>
         </div>
 
     );
